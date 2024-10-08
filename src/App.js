a69d24ea66e68df23b13a5482087e9a617e1a97e
Pietro Comelli <comelli.pietro@gamil.com>
2024-10-08 17:41:54 +0200

    app.js, header.css, header.js, gif.webp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Header from './Header';
+import myGif from './gif.webp';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header /> {/**/ }
+      <main>
+        <h2>Questi qua siamo io e te!</h2>
+        <p>TI AMO TANTO BABY</p>
+        <img src={myGif} alt="Animazione GIF"/>
+      </main>
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Header.css';
+
+function Header(){
+    return (
+        <header>
+            <h1>Ciao cucciola mia!</h1>
+            <nav>
+                <ul>
+                    {/*<li><a href="#home">Home</a></li>
+                    <li><a href="#about">Chi Siamo</a></li>
+                    <li><a href="#contact">Contatti</a></li>*/}
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
